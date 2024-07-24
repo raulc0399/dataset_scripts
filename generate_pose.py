e6654195aa89988479613e4046349d2ae51fcc98
raci0399 <raci0399@gmail.com>
2024-07-24 20:04:25 +0200

    pose script

---
+++ generate_pose.py
@@ -0,0 +1,12 @@
+from controlnet_aux.processor import OpenposeDetector
+from PIL import Image
+
+open_pose = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
+
+folder = "./images"
+
+for i in range(10):
+    img = Image.open(f"{folder}/{i}.jpg").convert("RGB").resize((512, 512))
+    processed_image_open_pose = open_pose(img, hand_and_face=True, detect_resolution=1024, image_resolution=1024)
+
+    processed_image_open_pose.save(f"{folder}/{i}_pose.jpg")
