c7e24a47aa15dd6025a1a3c1bf69921c1803427c
raci0399 <raci0399@gmail.com>
2024-07-24 20:13:38 +0200

    file and dependency

--- generate_pose.py
+++ generate_pose.py
@@ -6,7 +6,7 @@ open_pose = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
 folder = "./images"
 
 for i in range(10):
-    img = Image.open(f"{folder}/{i}.jpg").convert("RGB").resize((512, 512))
+    img = Image.open(f"{folder}/{i+1}.jpg").convert("RGB").resize((512, 512))
     processed_image_open_pose = open_pose(img, hand_and_face=True, detect_resolution=1024, image_resolution=1024)
 
-    processed_image_open_pose.save(f"{folder}/{i}_pose.jpg")
+    processed_image_open_pose.save(f"{folder}/{i+1}_pose.jpg")
