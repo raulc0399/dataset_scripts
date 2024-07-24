7861b6f1af91bc7cbb5c8eeea1a011dac8f0f60c
raci0399 <raci0399@gmail.com>
2024-07-24 20:23:10 +0200

    if no pose detected, do not save file

--- generate_pose.py
+++ generate_pose.py
@@ -2,11 +2,18 @@ from controlnet_aux.processor import OpenposeDetector
 from PIL import Image
 
 open_pose = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
+open_pose = open_pose.to("cuda")
 
 folder = "./test_images"
 
 for i in range(10):
-    img = Image.open(f"{folder}/{i+1}.jpg").convert("RGB").resize((512, 512))
+    file_name = f"{i+1}.jpg"
+    print(f"Processing {file_name}")
+
+    img = Image.open(f"{folder}/{file_name}").convert("RGB").resize((512, 512))
     processed_image_open_pose = open_pose(img, hand_and_face=True, detect_resolution=1024, image_resolution=1024)
 
-    processed_image_open_pose.save(f"{folder}/{i+1}_pose.jpg")
+    if processed_image_open_pose is None:
+        print(f"No pose detected for {file_name}")
+    else:
+        processed_image_open_pose.save(f"{folder}/{i+1}_pose.jpg")
