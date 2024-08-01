800d1fc0e54fbcc9b0cf3eac9960ef390e50a5ed
raci0399 <raci0399@gmail.com>
2024-08-01 09:25:38 +0200

    save parquet file in chunks

--- convert_to_parquet.py
+++ convert_to_parquet.py
@@ -4,35 +4,44 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
+def count_lines(file_path):
+    with open(file_path, 'rb') as f:
+        return sum(1 for _ in f)
+
 def convert_json_to_parquet_with_images(json_file, parquet_file, images_folder):
-    chunk_size = 500
+    chunk_size = 150
     data = []
+
+    # Calculate the maximum number of chunks
+    total_lines = count_lines(json_file)
+    max_nr = (total_lines + chunk_size - 1) // chunk_size
+    chunk_index = 0
+
+    print(f"Converting {total_lines} lines to parquet with chunk size {chunk_size} and {max_nr} chunks")
 
     # Read JSON file and process in chunks
     with open(json_file, 'r') as f:
         for line in tqdm(f, desc="Reading JSON file"):
             data.append(json.loads(line))
             if len(data) == chunk_size:
-                process_chunk(data, images_folder, parquet_file)
+                process_chunk(data, images_folder, parquet_file, chunk_index, max_nr)
+                chunk_index += 1
                 data = []
 
     # Process any remaining data
     if data:
-        process_chunk(data, images_folder, parquet_file)
+        process_chunk(data, images_folder, parquet_file, chunk_index, max_nr)
 
-def process_chunk(data, images_folder, parquet_file):
+def process_chunk(data, images_folder, parquet_file, chunk_index, max_nr):
     # Convert chunk to DataFrame
     df = pd.DataFrame(data)
 
     for column in ['image', 'conditioning_image']:
         df[column] = df[column].apply(lambda x: Image.open(os.path.join(images_folder, x)).tobytes())
 
-    # Create/Append to Parquet file
-    if not os.path.exists(parquet_file):
-        df.to_parquet(parquet_file, engine='fastparquet')
-        exit()
-    else:
-        df.to_parquet(parquet_file, engine='fastparquet', append=True)
+    # Create
+    parquet_file_with_index = parquet_file.replace('.parquet', f'-{chunk_index:05d}-of-{max_nr:05d}.parquet')
+    df.to_parquet(parquet_file_with_index, engine='pyarrow')
 
 def convert_json_to_parquet(json_file, parquet_file):
     # Read JSON file
@@ -50,8 +59,12 @@ def convert_json_to_parquet(json_file, parquet_file):
 # Usage
 images_folder = '../persons-photo-concept-bucket-images-to-train/open_pose_controlnet/images'
 json_file = '../persons-photo-concept-bucket-images-to-train/open_pose_controlnet/images/metadata.jsonl'
-parquet_file = '../persons-photo-concept-bucket-images-to-train/open_pose_controlnet/train.parquet'
+parquet_files_folder = '../persons-photo-concept-bucket-images-to-train/open_pose_controlnet/data'
 
-# convert_json_to_parquet_with_images(json_file, parquet_file, images_folder)
-convert_json_to_parquet(json_file, parquet_file)
-print(f"Conversion complete. Parquet file saved as {parquet_file}")
+os.makedirs(parquet_files_folder, exist_ok=True)
+
+parquet_file = os.path.join(parquet_files_folder, 'train.parquet')
+
+convert_json_to_parquet_with_images(json_file, parquet_file, images_folder)
+# convert_json_to_parquet(json_file, parquet_file)
+print(f"Conversion complete. Parquet files saved with pattern {parquet_file.replace('.parquet', '-<chunk_index>-of-<max_nr>.parquet')}")
