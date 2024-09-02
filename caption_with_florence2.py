4c9bca0404fbb052c163460681774c91c71aaf61
raci0399 <raci0399@gmail.com>
2024-09-02 19:45:45 +0200

    results for joycaption caption with florence

---
+++ caption_with_florence2.py
@@ -0,0 +1,40 @@
+import requests
+
+import torch
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForCausalLM 
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+MODEL_ID = "microsoft/Florence-2-large"
+
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, torch_dtype=torch_dtype, trust_remote_code=True).to(device)
+processor = AutoProcessor.from_pretrained(MODEL_ID, trust_remote_code=True)
+
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
+image = Image.open(requests.get(url, stream=True).raw)
+
+def run_example(task_prompt, text_input=None):
+    if text_input is None:
+        prompt = task_prompt
+    else:
+        prompt = task_prompt + text_input
+
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to(device, torch_dtype)
+
+    generated_ids = model.generate(
+      input_ids=inputs["input_ids"],
+      pixel_values=inputs["pixel_values"],
+      max_new_tokens=1024,
+      num_beams=3
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+
+    parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
+
+    print(parsed_answer)
+
+
+prompt = "<MORE_DETAILED_CAPTION>"
+run_example(prompt)
