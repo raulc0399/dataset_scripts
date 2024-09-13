319748875527fe9dd9ed80626a040fc110850fa1
raci0399 <raci0399@gmail.com>
2024-09-13 17:25:50 +0200

    test run option

--- run_joycaption.sh
+++ run_joycaption.sh
@@ -4,5 +4,8 @@
 TRIGGER="your_trigger_word_or_sentence"
 IMAGE_DIR="./path_to_your_image_directory"
 
+# Test run variable
+TEST_RUN=false
+
 # Run the caption generation script
-python caption_with_joycaption.py --trigger "$TRIGGER" --image-dir "$IMAGE_DIR"
+python caption_with_joycaption.py --trigger "$TRIGGER" --image-dir "$IMAGE_DIR" $( [ "$TEST_RUN" = true ] && echo "--test-run" )
