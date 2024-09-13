596e3bf9d0b1fc80049fc090d0366ce7fdbd562a
raci0399 <raci0399@gmail.com>
2024-09-13 20:55:55 +0200

    exec flag

--- run_florence2.sh
+++ run_florence2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Variables
-TRIGGER="your_trigger_word_or_sentence"
-IMAGE_DIR="./path_to_your_image_directory"
+TRIGGER="oxen_style"
+IMAGE_DIR="../teenyicons/to_train"
 
 # Test run variable
-TEST_RUN=false
+TEST_RUN=true
 
 # Run the caption generation script
 python caption_with_florence2.py --trigger "$TRIGGER" --image-dir "$IMAGE_DIR" $( [ "$TEST_RUN" = true ] && echo "--test-run" )
