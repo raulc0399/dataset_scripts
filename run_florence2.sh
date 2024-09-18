6921c31e983496c0492dab65b0837d1129bad359
raci0399 <raci0399@gmail.com>
2024-09-18 13:17:22 +0200

    output dir to sh files as well

--- run_florence2.sh
+++ run_florence2.sh
@@ -7,5 +7,8 @@ IMAGE_DIR="../teenyicons/to_train"
 # Test run variable
 TEST_RUN=true
 
+# Output directory variable
+OUTPUT_DIR=""
+
 # Run the caption generation script
-python caption_with_florence2.py --trigger "$TRIGGER" --image-dir "$IMAGE_DIR" $( [ "$TEST_RUN" = true ] && echo "--test-run" )
+python caption_with_florence2.py --trigger "$TRIGGER" --image-dir "$IMAGE_DIR" $( [ -n "$OUTPUT_DIR" ] && echo "--output-dir $OUTPUT_DIR" ) $( [ "$TEST_RUN" = true ] && echo "--test-run" )
