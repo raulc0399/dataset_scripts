a48052f73a437d6d57483b2896cd8d6fa6e82ba1
raulc0399 <raci0399@gmail.com>
2025-01-07 19:16:55 +0100

    different depth images

--- gen_depth.py
+++ gen_depth.py
@@ -1,9 +1,22 @@
-from controlnet_aux.processor import MidasDetector
+from controlnet_aux.processor import MidasDetector, ZoeDetector, Processor
 from PIL import Image
+import os
+
+home_dir = os.path.expanduser("~")
+img = Image.open(os.path.join(home_dir, "tmp/input_img.png"))
 
 midas = MidasDetector.from_pretrained("lllyasviel/Annotators")
-
-img = Image.open("~/tmp/spaceship.png")
 depth_image = midas(img)
+depth_image.save(os.path.join(home_dir, "tmp/controlnet_img_midas.png"))
 
-depth_image.save("~/tmp/spaceship_depth.png")
+zoe = ZoeDetector.from_pretrained("lllyasviel/Annotators")
+depth_image1 = zoe(img)
+depth_image1.save(os.path.join(home_dir, "tmp/controlnet_img_zoe.png"))
+
+p = Processor("depth_leres")
+depth_image2 = p(img)
+depth_image2.save(os.path.join(home_dir, "tmp/controlnet_img_leres.png"))
+
+p1 = Processor("depth_leres++")
+depth_image3 = p1(img)
+depth_image3.save(os.path.join(home_dir, "tmp/controlnet_img_lerespp.png"))
